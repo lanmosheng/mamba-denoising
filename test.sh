f576e3e2ef7b954123354016e62f918f648dca80
lanmosheng <117844386+lanmosheng@users.noreply.github.com>
2025-09-11 13:46:47 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-# python eval.py --profile profile/eval/s_i1_base.txt --mode face --weighted --csv resnet_results.csv
-# python eval.py --profile profile/eval/s_i1_new.txt --mode face --weighted --csv mamba_results.csv
-# python compare.py --new mamba_results.csv --base resnet_results.csv --labels "Mamba,ResNet"
-
-python eval.py --profile profile/eval/s_i2_base.txt --mode face --weighted --csv resnet_results2.csv
-python eval.py --profile profile/eval/s_i2_new.txt --mode face --weighted --csv mamba_results2.csv
-python compare.py --new mamba_results2.csv --base resnet_results2.csv --labels "Mamba,ResNet"
